0039bad37c01380895d348d042ba7dffd1e9cd2a
KAZEFKK <18872326129@163.com>
2025-04-03 16:19:39 +0800

    second save

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import buttona from './components/buttona.vue'
+console.log();
 
 const app = createApp(App)
 app.component('buttona', buttona)  // 正确的注册方式
